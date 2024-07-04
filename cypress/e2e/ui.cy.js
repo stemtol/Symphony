ea3168ca61e06b574b2e4f589080bba0c4236169
Tsanyaolu <tsanyaolu@alethian.com>
2024-07-04 16:31:46 +0100

    ui.cy.js update

--- cypress/e2e/ui.cy.js
+++ cypress/e2e/ui.cy.js
@@ -1,29 +1,33 @@
 describe('SauceDemo Sorting Tests', () => {
     const URL = 'https://www.saucedemo.com/';
 
-    beforeEach(() => {
+    it('should verify items are sorted by name (A -> Z)', () => {
         cy.fixture('loginDetails.json').then(loginDetails => {
             cy.visit(URL);
             cy.get('input#user-name').type(loginDetails.username);
             cy.get('input#password').type(loginDetails.password);
             cy.get('input#login-button').click();
-        });
-    });
 
-    it('should verify items are sorted by name (A -> Z)', () => {
-        cy.get('.inventory_item_name').then(items => {
-            const itemNames = items.map((index, item) => item.innerText).get();
-            expect(itemNames).to.deep.equal([...itemNames].sort());
+            cy.get('.inventory_item_name').then(items => {
+                const itemNames = items.map((index, item) => item.innerText).get();
+                expect(itemNames).to.deep.equal([...itemNames].sort());
+            });
         });
     });
 
     it('should change sorting to name (Z -> A) and verify items are sorted correctly', () => {
-        cy.get('.product_sort_container').select('za');
-        cy.wait(2200)
-        cy.get('.inventory_item_name').then(items => {
-            const itemNames = items.map((index, item) => item.innerText).get();
-            expect(itemNames).to.deep.equal([...itemNames].sort().reverse());
+        cy.fixture('loginDetails.json').then(loginDetails => {
+            cy.visit(URL);
+            cy.get('input#user-name').type(loginDetails.username);
+            cy.get('input#password').type(loginDetails.password);
+            cy.get('input#login-button').click();
+
+            cy.get('.product_sort_container').select('za');
+            cy.wait(2200);
+            cy.get('.inventory_item_name').then(items => {
+                const itemNames = items.map((index, item) => item.innerText).get();
+                expect(itemNames).to.deep.equal([...itemNames].sort().reverse());
+            });
         });
     });
-
 });
